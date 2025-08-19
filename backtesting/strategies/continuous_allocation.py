60fe73d6dbe6c2ac18259259242fa9d64de3c2df
Sparros <sean.parro@gmail.com>
2025-08-19 17:21:52 +0100

    continued backtesting

--- backtesting/strategies/continuous_allocation.py
+++ backtesting/strategies/continuous_allocation.py
@@ -1,35 +1,37 @@
 from backtesting import Strategy
+import pandas as pd
 
 class ContinuousAllocationStrategy(Strategy):
     allocation_percentage = 0.5
-    _slippage = 0.0005  
+    sl_percentage = 0.05
+    slippage = 0.0005
 
     def init(self):
-        self.signal = self.data.Prediction
+        # Store the prediction signal
+        self.signal = self.data['Prediction']
 
     def next(self):
         prediction = self.signal[-1]
         current_account_value = self.equity
         current_position_value = self.position.size * self.data.Close[-1]
 
-        # Determine target position value
         if prediction == 1:
             target_position_value = current_account_value * self.allocation_percentage
         else:
             target_position_value = 0
 
-        # Buy logic
         if target_position_value > current_position_value:
             amount_to_buy = target_position_value - current_position_value
-            buy_price = self.data.Close[-1] * (1 + self._slippage)
+            buy_price = self.data.Close[-1] * (1 + self.slippage)
             shares_to_buy = int(amount_to_buy / buy_price)
             if shares_to_buy > 0:
-                self.buy(size=shares_to_buy)
+                # Add trailing stop (fixed % below entry)
+                sl_price = self.data.Close[-1] * (1 - self.sl_percentage)
+                self.buy(size=shares_to_buy, sl=sl_price)
 
-        # Sell logic
         elif target_position_value < current_position_value:
             amount_to_sell = current_position_value - target_position_value
-            sell_price = self.data.Close[-1] * (1 - self._slippage)
+            sell_price = self.data.Close[-1] * (1 - self.slippage)
             shares_to_sell = int(amount_to_sell / sell_price)
             if shares_to_sell > 0:
                 self.sell(size=shares_to_sell)
